6cc0d14fe6fc500300268e16a736ddca5f611bb2
Fellipe Developer <133868219+fefeprogram2001@users.noreply.github.com>
2023-12-14 18:25:36 -0300

    fib.cpp

--- fib.cpp
+++ fib.cpp
@@ -2,19 +2,18 @@
 using namespace std;
 
 int recursive_function(int n) {
-  if(n == 0) {
-    return 0;
-  } else if(n == 1) {
-    return 1;
-  } else {
-    return recursive_function(n - 1) + recursive_function(n - 2);
-  }
+    if(n == 0) {
+        return 0;
+    } else if(n == 1) {
+        return 1;
+    } else {
+        return recursive_function(n - 1) + recursive_function(n - 2);
+    }
 }
 
 int main() {
-  int n = 10;
-  int function;
-  function = recursive_function(n);
-  cout << "Result: " << function;
-  return 0;
+    int n = 10;
+    int result = recursive_function(n);
+    cout << "Result = " << result;
+    return 0;
 }
